7dedd710f874d3ae2093861942c4e0135bfd2f6e
hassanecoulibaly <hassancoul@gmail.com>
2020-12-31 16:07:56 +0000

    Implement map based service

---
+++ pet-clinic-data/src/main/java/com/devhas/devhasspringpetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,33 @@
+package com.devhas.devhasspringpetclinic.services.map;
+
+import com.devhas.devhasspringpetclinic.models.Vet;
+import com.devhas.devhasspringpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+}
