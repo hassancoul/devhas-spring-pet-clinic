b9dac2dbb8c29481d73eb518025f1d54045a0d8b
hassanecoulibaly <hassancoul@gmail.com>
2020-12-31 21:48:54 +0000

    Adding index page and controller

---
+++ pet-clinic-web/src/main/java/com/devhas/devhasspringpetclinic/indexController.java
@@ -0,0 +1,13 @@
+package com.devhas.devhasspringpetclinic;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class indexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index() {
+        return "index";
+    }
+}
