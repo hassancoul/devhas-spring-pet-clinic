21ba4bf6515300c62f5846c05348e0c2904b91fe
hassanecoulibaly <hassancoul@gmail.com>
2021-03-01 10:24:19 +0000

    Create Speciality and updating Vet.

--- pet-clinic-data/src/main/java/com/devhas/devhasspringpetclinic/models/Vet.java
+++ pet-clinic-data/src/main/java/com/devhas/devhasspringpetclinic/models/Vet.java
@@ -1,4 +1,15 @@
 package com.devhas.devhasspringpetclinic.models;
 
+import java.util.Set;
+
 public class Vet extends Person {
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
