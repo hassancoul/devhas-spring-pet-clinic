beb3ace8c0004f3830daf4be17dde38dd8c805bf
hassanecoulibaly <hassancoul@gmail.com>
2021-05-02 21:16:30 +0000

    updating owner with jpa annotations

--- pet-clinic-data/src/main/java/com/devhas/devhasspringpetclinic/models/PetType.java
+++ pet-clinic-data/src/main/java/com/devhas/devhasspringpetclinic/models/PetType.java
@@ -1,7 +1,14 @@
 package com.devhas.devhasspringpetclinic.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "type")
 public class PetType extends BaseEntity {
 
+    @Column(name = "name")
     private String name;
 
     public String getName() {
