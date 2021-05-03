7d3122def574dddf67cdfd64279c8fa28d6a2f9f
hassanecoulibaly <hassancoul@gmail.com>
2021-05-03 01:25:47 +0000

    creating spring data jpa for vet

---
+++ pet-clinic-data/src/main/java/com/devhas/devhasspringpetclinic/services/springdatajpa/VetSDJpaService.java
@@ -0,0 +1,46 @@
+package com.devhas.devhasspringpetclinic.services.springdatajpa;
+
+import com.devhas.devhasspringpetclinic.models.Vet;
+import com.devhas.devhasspringpetclinic.repositories.VetRepository;
+import com.devhas.devhasspringpetclinic.services.VetService;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class VetSDJpaService implements VetService {
+
+    private final VetRepository vetRepository;
+
+    public VetSDJpaService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return vetRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet vet) {
+        return vetRepository.save(vet);
+    }
+
+    @Override
+    public void delete(Vet vet) {
+        vetRepository.delete(vet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        vetRepository.deleteById(id);
+    }
+}
