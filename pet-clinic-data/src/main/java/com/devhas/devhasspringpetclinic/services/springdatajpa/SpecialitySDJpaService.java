56edb2191ec4480860e4dda7619322c36777c031
hassanecoulibaly <hassancoul@gmail.com>
2021-05-03 01:33:10 +0000

    creating spring data jpa for speciality

---
+++ pet-clinic-data/src/main/java/com/devhas/devhasspringpetclinic/services/springdatajpa/SpecialitySDJpaService.java
@@ -0,0 +1,44 @@
+package com.devhas.devhasspringpetclinic.services.springdatajpa;
+
+import com.devhas.devhasspringpetclinic.models.Speciality;
+import com.devhas.devhasspringpetclinic.repositories.SpecialityRepository;
+import com.devhas.devhasspringpetclinic.services.SpecialityService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SpecialitySDJpaService implements SpecialityService {
+
+    private final SpecialityRepository specialityRepository;
+
+    public SpecialitySDJpaService(SpecialityRepository specialityRepository) {
+        this.specialityRepository = specialityRepository;
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        Set<Speciality> specialities = new HashSet<>();
+        specialityRepository.findAll().forEach(specialities::add);
+        return specialities;
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return specialityRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Speciality save(Speciality speciality) {
+        return specialityRepository.save(speciality);
+    }
+
+    @Override
+    public void delete(Speciality speciality) {
+        specialityRepository.delete(speciality);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        specialityRepository.deleteById(id);
+    }
+}
