89bbb2eca975247bd18889223e15d5cdd0308419
hassanecoulibaly <hassancoul@gmail.com>
2021-05-10 19:20:01 +0000

    update to junit5

--- pet-clinic-data/src/main/java/com/devhas/devhasspringpetclinic/models/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/devhas/devhasspringpetclinic/models/BaseEntity.java
@@ -17,12 +17,4 @@ public class BaseEntity implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
